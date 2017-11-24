59cd7cf0fd0171b9ea8122bac087be80e0993b16
mynian <mynian@gmail.com>
2017-11-24 12:55:51 -0500

    Update XmogSetHide.lua

--- XmogSetHide.lua
+++ XmogSetHide.lua
@@ -1,1 +1,27 @@
-WardrobeCollectionFrameTab2:Hide()
+local Addon, events = CreateFrame("Frame", "xmogsethide"), {};
+
+local function hidesets(self)
+	WardrobeCollectionFrameTab2:SetScript("OnEvent", nil)
+	WardrobeCollectionFrameTab2:Hide()
+end
+
+local function showsets(self)
+	WardrobeCollectionFrameTab2:SetScript("OnEvent", WardrobeCollectionFrameTab2_OnEvent)
+	WardrobeCollectionFrameTab2:Show()
+end
+
+function events:TRANSMOGRIFY_OPEN(...)
+	hidesets(self)
+end
+
+function events:TRANSMOGRIFY_CLOSE(...)
+	showsets(self)
+end
+
+Addon:SetScript("OnEvent", function(self, event, ...)
+ events[event](self, ...); 
+end);
+
+for k, v in pairs(events) do
+ Addon:RegisterEvent(k);
+end
